2585e3ce1d8761458f810be20e01898e5679a4fd
al-arafat75 <alarafat2004@gmail.com>
2021-12-31 08:35:06 +0600

    first commit

---
+++ book.js
@@ -0,0 +1,44 @@
+const searchBook= async ()=>{
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    // console.log(searchText);
+    searchField.value='';
+    if(searchText==''){
+      // please write something to display
+    }
+    else{
+       const url=`https://openlibrary.org/search.json?q=${searchText}`
+    const res=await fetch(url);
+    const data=await res.json();
+    displaySearchResult(data.docs)
+
+    // fetch(url)
+    // .then(res=>res.json())
+    // .then(data=>displaySearchResult(data.meals))
+
+    }
+}
+
+const displaySearchResult=(books)=>{
+    const searchResult=document.getElementById('search-result');
+    books.forEach(book=>{
+        console.log(book.cover_i)
+        const div=document.createElement('div')
+       div.classList.add('col-md-3');
+      const imageUrl = "https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg";
+      console.log(imageUrl);
+       div.innerHTML=`
+       <div class="shadow rounded p-3 m-2">
+    <img src=${imageUrl} alt="" />
+       <h3>${book.title}</h3>
+       <p>${book.first_publish_year}</p>
+       
+   </div>
+       `;
+       searchResult.appendChild(div);
+       
+
+    })
+   
+
+}
